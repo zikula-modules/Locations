4252eaab564e6c421ede042aeab1a7eae263a251
hvorragend <carsten@zikula.org>
2010-04-10 17:25:34 +0000

    I will never click on 'format source' for templates in Eclipse again :-(

--- pnjavascript/locations.js
+++ pnjavascript/locations.js
@@ -1,10 +1,10 @@
 function livesearch(field, chars)
 {
     new Ajax.Autocompleter(field, field+'_choices', document.location.pnbaseURL + 'ajax.php?module=locations&func=get&field='+field,
-                           {paramName: 'fragment',
-                            minChars: chars,
-                            indicator: 'ajax_indicator',
-                            frequency: 0.1
-                            }
-                            );
+    {paramName: 'fragment',
+        minChars: chars,
+        indicator: 'ajax_indicator',
+        frequency: 0.1
+    }
+    );
 }
